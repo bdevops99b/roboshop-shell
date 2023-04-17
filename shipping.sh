505fdf4eddae1ea56abe2301a75d5b0d192cc542
bdevops99 <bdevops99@gmail.com>
2023-04-17 22:56:26 +0530

    catalogue scripts

--- shipping.sh
+++ shipping.sh
@@ -7,6 +7,7 @@ unzip /tmp/shipping.zip
 cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+cp /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
 
 systemctl daemon-reload
 systemctl enable shipping
