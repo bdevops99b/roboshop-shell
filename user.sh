a32f76825ef21c8e5a783b651954380c151f4e4b
bdevops99 <bdevops99@gmail.com>
2023-04-18 23:26:46 +0530

    Variables usage

--- user.sh
+++ user.sh
@@ -1,6 +1,10 @@
+script_path=$(dirname 0)
+source $(script_path)/common.sh
+useradd ${app_user}
+exit
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-useradd roboshop
+useradd ${app_user}
 mkdir /app
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
