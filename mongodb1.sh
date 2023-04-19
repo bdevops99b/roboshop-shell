0f25dc88d65100f40000e1f20d89c6555e031af1
bdevops99 <bdevops99@gmail.com>
2023-04-19 17:04:32 +0530

    Variables usage

--- mongodb1.sh
+++ mongodb1.sh
@@ -1,4 +1,5 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+script_path=$(dirname $0)
+cp  ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
