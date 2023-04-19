5a6b5ef7d9faa9771be58a83e29e9e729dbc228b
bdevops99 <bdevops99@gmail.com>
2023-04-19 22:32:42 +0530

    Variables usage

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,12 @@
 script=$(realpath "$0")
-#realpath $0
+
+realpath $0
+
+exit
 script_path=$(dirname "$realpath")
 #echo $script_name1
 #echo $script
 
-script_path=$(dirname $0)
 source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
