39cc97cf134b1bd12d533909b3fcad2b3fcd22c9
bdevops99 <bdevops99@gmail.com>
2023-04-20 17:14:07 +0530

    Variables usage

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,5 +1,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+
+exit
+
 source ${script_path}/common.sh
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 yum install erlang -y
