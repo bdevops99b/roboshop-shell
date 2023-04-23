0278322e658ba86ca52e9d8b0e9779b27a8ab042
bdevops99 <bdevops99@gmail.com>
2023-04-23 23:14:52 +0530

    functions

--- common.sh
+++ common.sh
@@ -1,33 +1,3 @@
 app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
-
-print_head() {
- echo -e "\e[33m>>>>>>>> $1 <<<<<<\e[0m"
-}
-func_nodejs()
-{
-print head "Configuring NodeJS repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
- print head "Installing NodeJS"
-yum install nodejs -y
- print head "Add Application User"
-print head "useradd ${app_user}"
-print head  "Create Application Directory"
-mkdir /app
-print head  "Download App Content"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-cd /app
-print head  "Unzip App Content"
-unzip /tmp/${component}.zip
-print head  "Install NodeJS Dependencies"
-npm install
-print head  "Copy Cart SystemD file"
-cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
- print head "Start Cart Service"
-systemctl daemon-reload
-systemctl enable cart
-systemctl start cart
-}
-
--- user.sh
+++ user.sh
@@ -1,12 +1,28 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-component=user
-func_nodejs
+echo -e "\e[36m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
+yum install nodejs -y
+echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
+useradd ${app_user}
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
+mkdir /app
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
+unzip /tmp/user.zip
+echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
+npm install
+echo -e "\e[36m>>>>>>>>> Copy user SystemD file <<<<<<<<\e[0m"
+cp $script_path/user.service /etc/systemd/system/user.service
+echo -e "\e[36m>>>>>>>>> Start user service   <<<<<<<<\e[0m"
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
 
-echo -e "\e[36m>>>>>>>>> Copy repos  file <<<<<<<<\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[36m>>>>>>>>> Install mongodb client <<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-echo -e "\e[36m>>>>>>>>> Load schema <<<<<<<<\e[0m"
 mongo --host mongod.panda4u.online </app/schema/user.js
