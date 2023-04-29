843128815c4de224d0312a86cfd9cec6d6e64c63
bdevops99 <bdevops99@gmail.com>
2023-04-29 16:02:54 +0530

    if conditions

--- common.sh
+++ common.sh
@@ -5,36 +5,50 @@ script_path=$(dirname "$script")
 func_print_head() {
   echo -e "\e[35m>>>>>>> $1 <<<<<<<\e[0m"
 }
+func_stat_check(){
+   if [ $1 -eq 0 ]; then
+        echo -e "\e[32mSuccess\e[0m"
+      else
+        echo -e "\e[31mFailure\e[0m"
+        exit 1
+      fi
+}
 func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
   print_head " Copy MongoDB repo "
   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+  func_stat_check $?
   print_head " Install MongoDB Client "
   yum install mongodb-org-shell -y
+  func_stat_check $?
   print_head "Load Schema "
   mongo --host mongod.panda4u.online </app/schema/${component}.js
+  func_stat_check $?
 fi
 
   if [ "$scheme_setup" == "mysql" ]; then
-  func_print_head " Download Maven Dependencies  "
-    mvn clean package
-    mv target/${component}-1.0.jar ${component}.jar
    func_print_head " Install Mysql client "
    yum install mysql -y
+   func_stat_check $?
    func_print_head " Load schema "
    mysql -h mysql.panda4u.online -uroot -p${mysql_root_password}< /app/schema/${component}.sql
+   func_stat_check $?
 fi
 }
 func_app_prereq(){
     func_print_head " Create Application User "
     useradd ${app_user}
+    func_stat_check $?
     func_print_head " Create Application Directory "
     mkdir /app
+    func_stat_check $?
     func_print_head " Download App Content "
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    func_stat_check $?
+    func_print_head " Extract Application Content "
     cd /app
-    func_print_head " Unzip App Content "
     unzip /tmp/${component}.zip
+    func_stat_check $?
 }
 
 func_systemd_setup(){
@@ -45,18 +59,22 @@ func_systemd_setup(){
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl start ${component}
+  func_stat_check $?
 }
 func_nodejs(){
   func_print_head " Configuring NodeJS repos "
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  func_stat_check $?
 
   func_print_head " Installing NodeJS "
   yum install nodejs -y
+  func_stat_check $?
 
   func_app_prereq
 
   func_print_head " Install NodeJS Dependencies "
   npm install
+  func_stat_check $?
 
   func_schema_setup
   func_systemd_setup
@@ -66,19 +84,11 @@ func_java(){
 
   func_print_head " Install maven "
   yum install maven -y
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSuccess\e[0m"
-  else
-    echo -e "\e[31mFailure\e[0m"
-  fi
+  func_stat_check $?
   func_app_prereq
   func_print_head " Download Maven Dependencies  "
   mvn clean package
-  if [ $? -eq 0 ]; then
-      echo -e "\e[32mSuccess\e[0m"
-    else
-      echo -e "\e[31mFailure\e[0m"
-    fi
+ func_stat_check $?
   mv target/${component}-1.0.jar ${component}.jar
 
   func_schema_setup
