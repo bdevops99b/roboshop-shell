4ddaf846ba42dd93039fbabb2abdb18afdf8657a
bdevops99 <bdevops99@gmail.com>
2023-04-29 13:06:19 +0530

    if conditions

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-#rabbitmq_appuser_password=$1
+rabbitmq_appuser_password=$1
 if [ -z "$rabbitmq_appuser_password" ]; then
   echo Input roboshop user password missing
  exit
