edd53762a89615ac4b10661c972f5b1c9899d11c
bdevops99 <bdevops99@gmail.com>
2023-04-29 14:37:50 +0530

    if conditions

--- common.sh
+++ common.sh
@@ -1,11 +1,11 @@
 app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
-
+# start
 print_head() {
   echo -e "\e[35m>>>>>>> $1 <<<<<<<\e[0m"
 }
-schema_setup(){
+func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
   print_head " Copy MongoDB repo "
   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
@@ -14,30 +14,63 @@ schema_setup(){
   print_head "Load Schema "
   mongo --host mongod.panda4u.online </app/schema/${component}.js
 fi
-}
-func_nodejs(){
-  print_head " Configuring NodeJS repos "
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head " Installing NodeJS "
-  yum install nodejs -y
-  print_head " Add Application User "
-  useradd ${app_user}
-  print_head " Create Application Directory "
-  mkdir /app
-  print_head " Download App Content "
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-  print_head " Unzip App Content "
-  unzip /tmp/${component}.zip
-  print_head " Install NodeJS Dependencies "
-  npm install
-  print_head " Copy ${component} SystemD file "
+  if [ "$scheme_setup" == "mysql" ]; then
+  func_print_head " Download Maven Dependencies  "
+    mvn clean package
+    mv target/${component}-1.0.jar ${component}.jar
+   func_print_head " Install Mysql client "
+   yum install mysql -y
+   func_print_head " Load schema "
+   mysql -h mysql.panda4u.online -uroot -p${mysql_root_password}< /app/schema/${component}.sql
+}
+func_app_prereq(){
+    func_print_head " Create Application User "
+    useradd ${app_user}
+    func_print_head " Create Application Directory "
+    mkdir /app
+    func_print_head " Download App Content "
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
+    func_print_head " Unzip App Content "
+    unzip /tmp/${component}.zip
+}
+
+func_systemd_setup(){
+  func_print_head " Setup SystemD service "
+  func_print_head " Copy ${component} SystemD file "
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-  print_head " Start ${component} Service "
+  func_print_head " Start ${component} services "
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl start ${component}
-
-  schema_setup
 }
+func_nodejs(){
+  func_print_head " Configuring NodeJS repos "
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+  func_print_head " Installing NodeJS "
+  yum install nodejs -y
+
+  func_app_prereq
+
+  func_print_head " Install NodeJS Dependencies "
+  npm install
+
+  func_schema_setup
+  func_systemd_setup
+}
+func_java(){
+
+  func_print_head " Install maven "
+  yum install maven -y
+
+  func_app_prereq
+  func_print_head " Download Maven Dependencies  "
+  mvn clean package
+  mv target/${component}-1.0.jar ${component} .jar
+
+  func_schema_setup
+  func_systemd_setup
+
+  }
--- shipping.sh
+++ shipping.sh
@@ -6,28 +6,7 @@ if [ -z "$mysql_root_password" ]; then
   echo Input mysql root password missing
  exit
 fi
-echo -e "\e[36m>>>>>>>>> Install maven <<<<<<<<\e[0m"
-yum install maven -y
-echo -e "\e[36m>>>>>>>>> Add application user  <<<<<<<<\e[0m"
-useradd ${app_user}
-echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
-mkdir /app
-echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[36m>>>>>>>>> Copy shipping SystemD file <<<<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[36m>>>>>>>>> Start shipping services<<<<<<<<\e[0m"
-
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
-
-yum install mysql -y
-mysql -h mysql.panda4u.online -uroot -p${mysql_root_password}< /app/schema/shipping.sql
-systemctl restart shipping
+#cmts
+component=shipping
+scheme_setup=mysql
+func_java
