fbdb41d5be87be7cab4e6ef9828f073928d93ed4
bdevops99 <bdevops99@gmail.com>
2023-04-29 18:18:36 +0530

    if conditions

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,24 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+
 func_print_head " Install Nginx server "
 yum install nginx -y &>>$log_file
+func_stat_check $?
 func_print_head " Copy roboshop configuration file "
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
-func_systemd_setup
+cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
 func_print_head " Remove old app content "
 rm -rf /usr/share/nginx/html/* &>>$log_file
-func_systemd_setup
+func_stat_check $?
 func_print_head "  Download App Content "
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-func_systemd_setup
+func_stat_check $?
 cd /usr/share/nginx/html &>>$log_file
 func_print_head " Unzip App Content "
 unzip /tmp/frontend.zip &>>$log_file
-func_systemd_setup
+func_stat_check $?
 func_print_head " Start nginx Service "
 systemctl restart nginx &>>$log_file
 systemctl enable nginx &>>$log_file
-func_systemd_setup
+func_stat_check $?
